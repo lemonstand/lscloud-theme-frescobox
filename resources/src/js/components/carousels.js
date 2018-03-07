1c1b5ee3581503b0cbca8eb932598e02331a0a05
Bruce Alderson <bruce@napkinware.com>
2018-03-06 21:53:26 -0800

    Moves JS to non-compiled locations.

--- resources/src/js/components/carousels.js
+++ resources/src/js/components/carousels.js
@@ -1,22 +1,37 @@
 !(function($) {
+  $(document).ready(function() {
+    $(".owl-carousel").each(function(index, element) {
+      var options = $(element).data("options");
 
-    $(document).ready(function() {
-
-        $('.owl-carousel').each(function(index, element) {
-            var options = $(element).data('options');            
-
-            $(element).owlCarousel($.extend({}, {
-                autoHeight: true,
-                dots: true,
-                items: 1,
-                nav: false,
-                navText: [
-                    '<span class="fa fa-chevron-left"></span>',
-                    '<span class="fa fa-chevron-right"></span>'
-                ]
-            }, options));
-        });
-
+      $(element).owlCarousel(
+        $.extend(
+          {},
+          {
+            autoHeight: true,
+            dots: true,
+            items: 1,
+            nav: false,
+            navText: [
+              '<span class="fa fa-chevron-left"></span>',
+              '<span class="fa fa-chevron-right"></span>'
+            ]
+          },
+          options
+        )
+      );
     });
 
+    $(".owl-stage").imagesLoaded(function() {
+      $(".owl-stage #owl-item active").owlCarousel({
+        items: 1,
+        nav: false,
+        pagination: false,
+        autoHeight: true,
+        loop: true,
+        lazyLoad: true,
+        animateOut: "slideOutRight",
+        animateIn: "pulse"
+      });
+    });
+  });
 })(jQuery);
