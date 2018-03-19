f7f7305cbb289d55769b1b66d2d63d9004997eb4
Bruce Alderson <bruce@napkinware.com>
2018-03-19 15:06:01 -0700

    Hooks reloading of product iamges and improves height load fix.

--- resources/src/js/components/carousels.js
+++ resources/src/js/components/carousels.js
@@ -1,27 +1,40 @@
 !(function($) {
   $(document).ready(function() {
-    $(".owl-carousel").each(function(index, element) {
-      var options = $(element).data("options"),
-        carousel = $(element);
-
-      carousel.owlCarousel(
-        $.extend(
-          {},
-          {
-            autoHeight: true,
-            dots: true,
-            items: 1,
-            nav: false,
-            navText: [
-              '<span class="fa fa-chevron-left"></span>',
-              '<span class="fa fa-chevron-right"></span>'
-            ]
-          },
-          options
-        )
-      );
-
-      carousel.trigger("refresh.owl.carousel"); // reflow carousel, fixes 1px preload bug
+    $(window).on("onAfterAjaxUpdate", function() {
+      hookCarousels();
+      $(".owl-carousel").trigger("refresh.owl.carousel");
     });
+
+    hookCarousels();
+
+    function hookCarousels() {
+      $(".owl-carousel:not(.hooked)").each(function(index, element) {
+        var options = $(element).data("options"),
+          carousel = $(element);
+
+        carousel.owlCarousel(
+          $.extend(
+            {},
+            {
+              autoHeight: true,
+              dots: true,
+              items: 1,
+              nav: false,
+              navText: [
+                '<span class="fa fa-chevron-left"></span>',
+                '<span class="fa fa-chevron-right"></span>'
+              ]
+            },
+            options
+          )
+        );
+        carousel.addClass("hooked");
+      });
+
+      setTimeout(function() {
+        // reflow carousel, fixes height preload bug
+        $(".owl-carousel.hooked").trigger("refresh.owl.carousel");
+      }, 250);
+    }
   });
 })(jQuery);
