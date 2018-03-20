cece4d1228b398e0079874aab5da9b0d5045085e
Bruce Alderson <bruce@napkinware.com>
2018-03-19 23:17:53 -0700

    Fixes apply coupon code.

--- resources/src/js/components/header.js
+++ resources/src/js/components/header.js
@@ -1,19 +1,18 @@
 !(function($) {
+  $(document).ready(function() {
+    $('[data-toggle="search"]').click(function(e) {
+      e.preventDefault();
+      $("#header-search").toggleClass("header-search-open");
 
-    $(document).ready(function() {
-
-        $('[data-toggle="search"]').click(function(e) {
-            e.preventDefault();
-            $('#header-search').toggleClass('header-search-open');
-            $('#header-search [name="query"]').focus();
-        });
-
-        $('.header-sticky').sticky({ 
-            topOffset: 0,
-            zIndex: 1040,
-            wrapperClassName: $('#header').is('.header-transparent') ? 'header-sticky-wrapper-transparent' : 'sticky-wrapper'
-        });
-        
+      $("#header-search input.query").focus();
     });
 
+    $(".header-sticky").sticky({
+      topOffset: 0,
+      zIndex: 1040,
+      wrapperClassName: $("#header").is(".header-transparent")
+        ? "header-sticky-wrapper-transparent"
+        : "sticky-wrapper"
+    });
+  });
 })(jQuery);
